dc6fd89c76d544185f627cb5e6f8ff751f889afb
Alex Kobylinski <oleksii.kobylinskyi@oracle.com>
2019-11-18 19:09:57 +0200

    added logic for the sample controller

--- src/main/java/com/yourproject/resource/sample/SampleController.java
+++ src/main/java/com/yourproject/resource/sample/SampleController.java
@@ -4,6 +4,7 @@ import com.yourproject.resource.constant.AuthorizationGrant;
 import com.yourproject.resource.model.mongo.Sample;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +25,9 @@ public class SampleController {
 
     private static final Logger LOG = LoggerFactory.getLogger(SampleController.class);
 
+    @Autowired
+    private SampleService sampleService;
+
     /**
      * Get Samples by Username.
      *
@@ -33,14 +37,31 @@ public class SampleController {
      * @return
      */
     @GetMapping(path = "/samples")
-    public ResponseEntity<String> getSamples(Principal principal,
+    public ResponseEntity<List<Sample>> getSamples(Principal principal,
                                              @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) Date startDate,
                                              @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) Date endDate,
+                                             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) Date date,
                                              @RequestParam(required = false) String title,
-                                             @RequestParam(required = false) String currencyTitle) {
+                                             @RequestParam(required = false) String currencyCode) {
 
+        String username = principal.getName();
 
+        if (title != null) {
+            return new ResponseEntity<>(this.sampleService.getSamplesByTitle(username, title), HttpStatus.OK);
+        }
 
-        return new ResponseEntity<>("", HttpStatus.OK);
+        if (date != null) {
+            return new ResponseEntity<>(this.sampleService.getSamplesByUsernameAndPreciseDate(username, date), HttpStatus.OK);
+        }
+
+        if (startDate != null && endDate != null) {
+            if (currencyCode != null) {
+                return new ResponseEntity<>(this.sampleService.getSamplesByUsernameAndDateAndCurrencyCode(username, startDate, endDate, currencyCode), HttpStatus.OK);
+            }
+
+            return new ResponseEntity<>(this.sampleService.getSamplesByOverlappingDateRangeAndUsername(username, startDate, endDate), HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(this.sampleService.getSamplesByUsername(username), HttpStatus.OK);
     }
 }
--- src/main/java/com/yourproject/resource/sample/SampleServiceImpl.java
+++ src/main/java/com/yourproject/resource/sample/SampleServiceImpl.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
