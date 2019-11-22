39ea02ae08cf9dc9f98dd8b6ca605faeb6f83bfa
Alex Kobylinski <oleksii.kobylinskyi@oracle.com>
2019-11-22 16:21:38 +0200

    removed extra exceptions and util methods

--- src/test/java/com/yourproject/resource/util/TestUtil.java
+++ src/test/java/com/yourproject/resource/util/TestUtil.java
@@ -1,28 +1,16 @@
 package com.yourproject.resource.util;
 
-import com.yourproject.resource.model.mongo.Currency;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.Calendar;
 import java.util.Date;
-import java.util.Map;
 
 import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.isEmptyString;
 
 public final class TestUtil {
-
-    public static final String CURRENCY_UAH_ID = "100500";
-    public static final String CURRENCY_USD_ID = "100600";
-    public static final String ANY_USERNAME = "WHOIS";
-
-    public static ResultMatcher getMatcherForOneEmptyString(String jsonPath) {
-        return MockMvcResultMatchers.jsonPath(jsonPath, isEmptyString());
-    }
-
     public static <T> ResultMatcher getMatcherForOneItem(String jsonPath, T expected) {
         return MockMvcResultMatchers.jsonPath(jsonPath, is(expected));
     }
@@ -33,17 +21,6 @@ public final class TestUtil {
 
     public static ResultMatcher getMatcherForHasSize(String jsonPath, int size) {
         return MockMvcResultMatchers.jsonPath(jsonPath, hasSize(size));
-    }
-
-    public static Currency getCurrency(String title) {
-        Currency uah = new Currency("EUR", "€");
-        uah.setId(CURRENCY_UAH_ID);
-
-        Currency usd = new Currency("USD", "$");
-        uah.setId(CURRENCY_USD_ID);
-
-        Map<String, Currency> currencies = Map.of("EUR", uah, "USD", usd);
-        return currencies.get(title);
     }
 
     public static Date addDays(Date date, int days) {
